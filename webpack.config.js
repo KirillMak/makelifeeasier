e3b6b8d7f07326bbd9b0362c4a55dd9cc65a7ed9
Kirill <makarih0@mail.ru>
2018-02-01 14:46:56 +0300

    changed htaccess

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ Encore
     // the project directory where compiled assets will be stored
     .setOutputPath('public/build/')
     // the public path used by the web server to access the previous directory
+    //.setPublicPath('/build')
     .setPublicPath('/build')
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
