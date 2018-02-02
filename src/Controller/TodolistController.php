0a4d240fd2c1251113716426df681164cc6185e3
Kirill <makarih0@mail.ru>
2018-02-02 16:09:35 +0300

    edit form

--- src/Controller/TodolistController.php
+++ src/Controller/TodolistController.php
@@ -50,9 +50,74 @@
        }
 
        
-       public function EditAction()
+       public function EditAction($id, Request $request)
        {
-        return $this->render('test.html.twig'); 
+            $task = $this->get('doctrine.orm.entity_manager')->getRepository(Task::class)->find($id);
+            //var_dump ($task);
+
+            //$task-> SetOwner($task -> getOwnerId());
+            //var_dump($task);
+            $task-> setTitle($task -> getTitle());
+            $task-> setDeadline($task -> getDeadline());
+            $task-> setContent($task-> getContent());
+            $task-> setAdditional($task ->getAdditional());
+            $task-> setCategory($task-> getCategory());
+            $task-> setPriority($task -> getPriority());
+            //$task-> setCreated($now);
+            //$task-> setIsDeleted($isDeleted);*/
+
+            $form = $this ->createFormBuilder($task)
+            ->add('title', TextType::class,array('attr'=> array('class' => 'form-control')))
+            ->add('deadline',DateTimeType::class,array('attr'=>array('class'=>'form-control')))
+            ->add('content',TextareaType::class, array('attr'=>array('class'=>'form-control','rows' => '10')))
+            ->add('additional',TextType::class, array('attr'=>array('class'=>'form-control')))
+            ->add('category',TextType::class, array('attr'=>array('class'=>'form-control')))
+            ->add('priority',ChoiceType::class, array('choices'=>array('Low'=>'1','Medium'=>'2','High'=>'3'),'attr'=>array('class'=>'form-control')))
+            ->add('save',SubmitType::class,array('label'=>'Редактировать задание','attr'=>array('class'=>'form-control')))
+            ->getForm();
+
+            //array('class'=>array('form-control','class'=> 'btn-success','class'=>'btn'))))
+
+            $form -> handleRequest($request);
+
+            if($form->isSubmitted() && $form->isValid() ){
+                //var_dump($_POST);
+               // var_dump($_SERVER);
+               $title = $form['title']->getData();
+               $deadline = $form['deadline']->getData();
+               $content = $form['content']->getData();
+               $additional = $form['additional']->getData();
+               $priority = $form['priority']->getData();
+               $category = $form['category']->getData();
+    
+               $isDeleted = false;
+               $owner_id = 1;
+               $now = new\DateTime('now');
+    
+               $task-> SetOwner($owner_id);
+               $task-> setTitle($title);
+               $task-> setDeadline($deadline);
+               $task-> setContent($content);
+               $task-> setAdditional($additional);
+               $task-> setCategory($category);
+               $task-> setPriority($priority);
+               $task-> setCreated($now);
+               $task-> setIsDeleted($isDeleted);
+              // var_dump($task);
+               $em = $this->getDoctrine()->getManager();
+    
+               $em->persist($task);
+               //var_dump($em);
+               $em->flush();
+               //var_dump($_POST);
+               //die ('test');
+            }
+
+            //return $this->render('test.html.twig'); 
+           
+            return $this->render('edit.html.twig', array(
+                'form'=> $form->createView()
+            )); 
        }
 
        public function AddAction(Request $request )
